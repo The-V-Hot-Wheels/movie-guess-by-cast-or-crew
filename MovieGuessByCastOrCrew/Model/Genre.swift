febbb5ff1002d10e2bab67866cf3bf9b03d24812
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-30 16:25:15 -0400

    New structs, enum

---
+++ MovieGuessByCastOrCrew/Model/Genre.swift
@@ -0,0 +1,22 @@
+//
+//  Genre.swift
+//  MovieGuessByCastOrCrew
+//
+//  Created by Alonso del Arte on 5/30/24.
+//
+
+import Foundation
+
+/// Enumerates movie genres. For now, this is limited to the movie genres in our
+/// main project.
+enum Genre {
+    
+    case action
+    
+    case comedy
+    
+    case drama
+    
+    case horror
+    
+}
---
+++ MovieGuessByCastOrCrew/Model/Movie.swift
@@ -0,0 +1,33 @@
+//
+//  Movie.swift
+//  MovieGuessByCastOrCrew
+//
+//  Created by Alonso del Arte on 5/30/24.
+//
+
+import Foundation
+
+struct Movie {
+    
+    /// The title of the movie. For example, *Shrek*.
+    let title: String
+    
+    /// The director of the movie. For example, Andrew Adamson and Vicky Jenson.
+    let director: String
+    
+    /// The author of the movie's screenplay. For example, William Steig, Ted
+    /// Elliott, Terry Rossio.
+    let writer: String
+    
+    /// The main stars of the movie. May be voice actors. For example, Mike
+    /// Myers, Eddie Murphy, Cameron Diaz.
+    let stars: String
+    
+    /// The genre of the movie. For example, comedy.
+    let genre: Genre
+    
+    /// The IMDb ID of the movie. Should generally consist of "tt" followed by a
+    /// zero-padded number. For example, "tt0126029".
+    let imdbID: String
+    
+}
